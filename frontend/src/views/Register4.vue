4303eaf723a6a693c79749d75fa7d97c114cbd77
Ron Borower <ronb168@walla.com>
2022-03-07 06:29:10 +0200

    improve register 4

--- frontend/src/views/Register4.vue
+++ frontend/src/views/Register4.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     <img alt="Couldn't load the image" src="../assets/miniLogo.jpeg">
-    <div v-if="counter<12">
+    <div v-if=" i < questions.length">
       <p>
-        {{ describes[counter] }}
+        {{ describes[i] }}
       </p>
       <h3>
-        {{ questions[counter] }}
+        {{ questions[i] }}
       </h3>
       <Button background-color="#519872" text="Yes" text-color="black" @click="saveAnswer('Yes')"/>
       <Button background-color="#E84855" text="No" text-color="black" @click="saveAnswer('No')"/>
     </div>
     <div v-else>
-      {{answers}}
       <router-link :to="{name: 'Register5'}">
         <Button background-color="#519872" text="Yes" text-color="black"/>
         <Button background-color="#E84855" text="No" text-color="black"/>
       </router-link>
+      {{ answers }}
     </div>
   </div>
 </template>
@@ -30,7 +30,7 @@ export default {
   components: {Button},
   setup() {
     let answers = ref([])
-    let counter = ref(0);
+    let i = ref(0);
     let describes = ref([
       "Some visual cues first...",
       "Some visual cues first...",
@@ -56,14 +56,14 @@ export default {
       "Does your cared rarely respond to being called by name?",
       "Does your cared scream or tends to not notice being very loud?",
       "Does you cared have a repulsion to being touched?",
-      "Is your cared insensitive to extreme temeratures or pain?",
+      "Is your cared insensitive to extreme temperatures or pain?",
       "Does your cared make constant physical contact without noticing?"
     ]);
     const saveAnswer = (answer) => {
-      counter.value++;
+      i.value++;
       answers.value.push(answer);
     };
-    return {questions, describes, counter, saveAnswer, answers}
+    return {questions, describes, i, saveAnswer, answers}
   }
 }
 </script>
