9eb8fad109477617b1f728a6b153d712733704a8
Ron Borower <ronb168@walla.com>
2022-04-04 12:44:41 +0300

    edit button

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header :style="style.design">
+    {{headerText}}
+  </header>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+  name: "Header",
+  setup(){
+    let headerText = ref("Hello User");
+    let fontSize = ref('25px');
+    let textColor = ref('white');
+    return {fontSize, textColor, headerText}
+  },
+  computed: {
+    style() {
+      let design = {
+        'font-size': this.fontSize,
+        'color': this.textColor,
+        'margin': '2vw',
+        'padding': '1vw',
+        'border': 'none'
+      }
+      return {design}
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
