e7440c437b978bddf63d3b8aed2b2341d5abd414
Ron Borower <ronb168@walla.com>
2022-04-05 23:13:50 +0300

    inputs component

---
+++ frontend/src/components/Input.vue
@@ -0,0 +1,37 @@
+<template>
+  <input value="text" :style="style.design">
+
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+  name: "Input",
+  props: ["text", "textColor", "backgroundColor"],
+  computed: {
+    style() {
+      let design = {
+        'background-color': this.backgroundColor,
+        'color': this.textColor,
+        'border-radius': '5px',
+        'margin': '2vw',
+        'padding': '1vw',
+        'border': 'none'
+      }
+      return {design}
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa&display=swap');
+input {
+  font-family: 'Comfortaa', cursive, sans-serif;
+  text-decoration: none;
+}
+input:hover{
+  cursor: pointer;
+}
+</style>
