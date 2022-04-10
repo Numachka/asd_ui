5510076771fcd76417113b116bf8b93cd2a1932b
Ron Borower <ronb168@walla.com>
2022-04-10 16:48:53 +0300

    post

--- frontend/src/views/Introduction.vue
+++ frontend/src/views/Introduction.vue
@@ -3,15 +3,16 @@
     Hi there! <br> Welcome to ASDUI. <br><br> This app is made for you, the caretakers to customize<br>
     and fit the user experience for your cared and help <br> them use their mobile phone to their full potential.
   </p>
-  <form>
+  <form @submit="postData" method="post">
     <p>
       Please enter your name
     </p>
-    <input type="text" required>
+    <input type="text" v-model="posts.Name" required>
     <p>
       Please enter your Email
     </p>
-    <input type="email">
+    <input type="email" v-model="posts.Email" required>
+    <Button type="submit" background-color="#FAA43E" text="submit" text-color="white"/>
   <router-link to="/register/explanation">
     <Button type="submit" background-color="#FAA43E" text="Next" text-color="white"/>
   </router-link>
@@ -19,9 +20,9 @@
 </template>
 
 <script>
-import Button from "../components/Button";
-import Input from "../components/Input";
-
+import Button from "../components/Button"
+import Input from "../components/Input"
+import {inject, ref} from 'vue'
 
 export default {
   name: "Introduction",
@@ -29,6 +30,22 @@ export default {
     Button,
     Input,
   },
+  setup(){
+    let posts = ref({
+      "Name": null,
+      "Email": null
+    });
+    let postData = (e) => {
+      this.axios.post("http://localhost:8080", posts.value)
+      .then((result) => {
+        console.log(result)
+      })
+      e.preventDefault();
+    };
+
+    return { posts, postData }
+  },
+
 }
 </script>
 
--- frontend/src/views/Questions.vue
+++ frontend/src/views/Questions.vue
@@ -1,7 +1,13 @@
 <template>
-  <div v-if=" i < questions.length">
-    <p>
-      {{ describes[i] }}
+  <div v-if= "i < questions.length">
+    <p v-if="i <= 3">
+      {{ describes[0] }}
+    </p>
+    <p v-else-if="i >= 4 && i <= 8">
+      {{ describes[1] }}
+    </p>
+    <p v-else>
+      {{ describes[2] }}
     </p>
     <h3>
       {{ questions[i] }}
@@ -17,20 +23,6 @@
     </router-link>
     {{ answers }}
   </div>
-<!--  <div>-->
-<!--    <img alt="Couldn't load the image" src="../assets/miniLogo.jpeg">-->
-<!--    <div v-for="(v,k) in describes1">-->
-<!--      <h1>-->
-<!--        {{k}}-->
-<!--      </h1>-->
-<!--      <h5 v-for="q in v">-->
-<!--        {{q}}-->
-<!--      <Button background-color="#519872" text="Yes" text-color="black" @click="saveAnswer(true)"/>-->
-<!--      <Button background-color="#E84855" text="No" text-color="black" @click="saveAnswer(false)"/>-->
-<!--      </h5>-->
-<!--    </div>-->
-<!--  </div>-->
-
 </template>
 
 <script>
@@ -43,42 +35,7 @@ export default {
   setup() {
     let answers = ref([])
     let i = ref(0);
-    let describes1 = ref({
-      "Some visual cues first...": [
-        "Is your cared sensitive to light?",
-        "Is your cared easily distracted by movement?",
-        "Does your cared easily lose focus on the task in front?",
-        "Does your cared tend to stare?"
-      ],
-      "Some auditory cues...": [
-        "Is your cared sensitive to sharp, loud noises?",
-        "Does background noise make your cared lose focus?",
-        "Does your cared tend to make steady, repeating noises? Ex. drumming, humming.",
-        "Does your cared rarely respond to being called by name?",
-        "Does your cared scream or tends to not notice being very loud?",
-        "Does you cared have a repulsion to being touched?"
-      ],
-      "Lastly, tactile cues...": [
-        "Does you cared have a repulsion to being touched?",
-        "Is your cared insensitive to extreme temperatures or pain?",
-        "Does your cared make constant physical contact without noticing?"
-      ]
-    });
     let describes = ref([
-      "Some visual cues first...",
-      "Some visual cues first...",
-      "Some visual cues first...",
-      "Some visual cues first...",
-      "Some auditory cues...",
-      "Some auditory cues...",
-      "Some auditory cues...",
-      "Some auditory cues...",
-      "Some auditory cues...",
-      "Lastly, tactile cues...",
-      "Lastly, tactile cues...",
-      "Lastly, tactile cues..."
-    ]);
-    let describes2 = ref([
       "Some visual cues first...",
       "Some auditory cues...",
       "Lastly, tactile cues..."
@@ -101,7 +58,7 @@ export default {
       i.value++;
       answers.value.push(answer);
     };
-    return {questions, describes, describes1, describes2, i, saveAnswer, answers}
+    return {questions, describes, i, saveAnswer, answers}
   }
 }
 </script>
